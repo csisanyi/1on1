7347c339d8f45fd782c470892a86c897e72f7df8
Sandor Csicsman <csisanyi@gmail.com>
2023-05-23 13:43:19 +0200

    mongo docker compose and mongo config added

--- src/main/java/com/getbridge/homework/HomeworkApplication.java
+++ src/main/java/com/getbridge/homework/HomeworkApplication.java
@@ -2,8 +2,9 @@ package com.getbridge.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class HomeworkApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/getbridge/homework/HomeworkApplicationTests.java
+++ src/test/java/com/getbridge/homework/HomeworkApplicationTests.java
@@ -1,13 +1,26 @@
 package com.getbridge.homework;
 
+import com.mongodb.client.MongoClient;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HomeworkApplicationTests {
 
+
+	@Autowired
+	private MongoClient mongoClient;
+
 	@Test
-	void contextLoads() {
+	void testMongoDBConnection() {
+		assert mongoClient != null;
+
+		// Test the connection by performing a simple operation
+		String databaseName = mongoClient.listDatabaseNames().first();
+		assert databaseName != null;
+		System.out.println("Connected to MongoDB: " + databaseName);
 	}
 
 }

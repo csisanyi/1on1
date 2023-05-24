24e3ae2ae6547c558ceafb4f07cd4e6f825fcc0f
Sandor Csicsman <csisanyi@gmail.com>
2023-05-24 20:06:02 +0200

    added controller + repos + swagger + some persistence testing

--- src/main/java/com/getbridge/homework/HomeworkApplication.java
+++ src/main/java/com/getbridge/homework/HomeworkApplication.java
@@ -3,9 +3,12 @@ package com.getbridge.homework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class HomeworkApplication {
+public class HomeworkApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkApplication.class, args);
